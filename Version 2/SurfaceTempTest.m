5e6b1e6279483698bbca778cdde031a92291d82c
IzJLea <izaak.lea@gmail.com>
2014-06-25 17:00:09 -0400

    Surface temperature fixes
    Fixed the majority of issues with k and with Clad outer temp

--- Version 2/SurfaceTempTest.m
+++ Version 2/SurfaceTempTest.m
@@ -18,7 +18,7 @@ doutclad=0.0138; %m
 
 roc=doutclad/2; %m
 
-dfuel=0.0131; %m
+dfuel=0.0122; %m
 
 tclad=0.00038; %m
 
@@ -41,14 +41,6 @@ Cp=[4.340;4.370;4.410;4.460;4.500;4.610;4.760;4.970;5.280;5.750;6.540;8.240;14.9
 
 kl=1e-3.*[0.680;0.677;0.673;0.669;0.663;0.650;0.632;0.609;0.581;0.548;0.509;0.469;0.427];
 
-% Zirconium emissivity temperature range
-
-%TezT=[100 150 200 300 400];
-
-% Zirconium emissivity
-
-%Tez=[0.424 0.414 0.416 0.434 0.433];
-
 % UO2 thermal conductivity temperatures
 
 kUO2T=400:100:2800;
@@ -64,6 +56,71 @@ mul=[0.170e-3;0.160e-3;0.150e-3;0.142e-3;0.134e-3;0.122e-3;0.111e-3;0.102e-3;0.0
 % liquid Prandtl Number
 
 Prl=[1.09;1.03;0.983;0.947;0.910;0.865;0.836;0.832;0.854;0.902;1.00;1.23;2.06];
+% Temperature for water properties (C)
+
+
+Tsat=[100
+110
+120
+130
+140
+150
+160
+170
+180
+190
+200
+210
+220
+230
+240
+250
+260
+270
+280
+290
+300
+310
+320
+330
+340
+350
+360
+370
+373.9500];
+
+% Fluid specific volume
+
+svfluid=[0.0010
+0.0011
+0.0011
+0.0011
+0.0011
+0.0011
+0.0011
+0.0011
+0.0011
+0.0011
+0.0012
+0.0012
+0.0012
+0.0012
+0.0012
+0.0013
+0.0013
+0.0013
+0.0013
+0.0014
+0.0014
+0.0014
+0.0015
+0.0016
+0.0016
+0.0017
+0.0019
+0.0022
+0.0031];
+
 
 
 
@@ -81,36 +138,38 @@ Prlcool=LinLook(TCp,Prl,Tbulk);
 
 mulcool=LinLook(TCp,mul,Tbulk);
 
-Recool=G*Dh/mulcool;
+rholcool=1/LinLook(Tsat, svfluid, Tbulk);
+
+Recool=M*rholcool*Dh/mulcool;
 
 Nucool=0.023*(Recool^0.8)*(Prlcool^0.3);
 
-hcool=Nucool*kcool/Lchannel; %kW/m^2.K
+hcool=Nucool*kcool/Dh; %kW/m^2.K
 
 Tclado=(Q/(hcool*doutclad*Lchannel*pi()))+Tbulk;
 
 %% Inner cladding temperature
-Tkzirc=300:100:1800;
-kzirc=1e-3*[13.41 13.99 14.74 15.67 16.79 18.08 19.55 21.21 23.04 25.05 27.24 29.61 32.16 34.89 37.80 40.89];
-kzircsys=LinLook(Tkzirc,kzirc,Tclado);
+kzirc=(7.51+(0.362e-3*Tclado)-(0.618e-7*Tclado^2)+(0.718e-11*Tclado^3))*10^-3;
 
 ric=roc-tclad;
 
-Tcladi=(Q*log(roc/ric)/(2*pi()*Lchannel*kzircsys))+Tclado;
+Tcladi=(Q*log(roc/ric)/(2*pi()*Lchannel*kzirc))+Tclado;
 
 %% Fuel pin outer temperature
 
-kgap=0.002e-3; %kW/m.C
+kgap=0.0476+(0.362e-3*Tcladi)-(0.618e-7*Tcladi^2)+(0.718e-11*Tcladi^3)*10^-3; %kW/m.C
 
-Qgap=(2*pi()*Lchannel*kgap*(Tf-Tcladi)/(log(r
+dman=ric-(dfuel/2); %m
 
-Tfuelo=(Q*log(ric/(dfuel/2))/(2*pi()*Lchannel*kgap))+Tcladi;
+djump=10e-6; %m
+
+hgap=kgap/(dman+djump);
+
+Tfuelo=(Q/(dfuel/2*Lchannel)/hgap)+Tcladi;
 
 %% Fuel pin centerline temperature
 
 diff=10;
-
-Guess=kUO20B(1);
 
 Tc=Tfuelo;
 
@@ -119,17 +178,19 @@ while diff>1
     Tave=(Tfuelo+Tc)/2;
     
     if Tave<400;
-        kUO2=Guess;
+        kUO2=kUO20B(1);
         
     else
         kUO2=LinLook(kUO2T,kUO20B,Tave);
         
     end
-    Tprev=Tc;
+   
     
-    Tc=(Q*(dfuel/2)^2/4/kUO2)+Tfuelo;
+    Tcnew=(Q*dfuel/2/4/kUO2)+Tfuelo;
     
-    diff=Tc-Tprev;
+    diff=Tcnew-Tc;
+    
+    Tc=Tcnew;
 end
 
 %% Temperature matrix creation
